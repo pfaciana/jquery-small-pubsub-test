79b543c5d2b082052d098be4ec18247b09618156
Phil Faciana <pfaciana@gmail.com>
2025-03-22 11:43:07 -0400

    Deploy website - based on 9a68f33afabf18809866204a63a709788997d0ad

---
+++ assets/js/12186b63.f36fe24d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[8296],{3566:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/jquery-small-pubsub-test/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.88e9f6cc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/jquery-small-pubsub-test/blog","blogTitle":"Blog","authorsListPath":"/jquery-small-pubsub-test/blog/authors"}')}}]);
